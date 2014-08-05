78b86fe232b0ce075ac0a06494b1feac764c4cfa
rsperberg <rsperberg@gmail.com>
2014-08-05 21:16:20 +0000

    Used jquery to populate the collection view

--- app/scripts/landing.js
+++ app/scripts/landing.js
@@ -4,6 +4,11 @@ $(document).ready(function() {
       $(this).text(subText + '!');
    });
 
+   /* Added for checkpoint 26  */
+$('.hero-content h3').hover(function(){
+      $(this).css('color','red');
+   });
+
    var onHoverAction = function(event) {
      console.log('Hover action triggered.');
      $(this).animate({'margin-top': '20px'});
@@ -15,4 +20,14 @@ $(document).ready(function() {
    };
 
 $('.selling-points .point').hover(onHoverAction, offHoverAction);
+/* Added for checkpoint 26  */
+$('.selling-points .point h5').click(function() {
+  console.log('Clicking.');
+  $(this).css('font-size', '24pt');
+});
+
+/* Added for checkpoint 26  */
+$('.logo ').click(function() {
+  $(this).fadeOut(2000);
+});
 });
