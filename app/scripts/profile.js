eb7e88b26fc792211ce98931e8900c05b281d949
rsperberg <rsperberg@gmail.com>
2014-08-18 18:05:06 +0000

    Added js to handle switching tabs on profile page

---
+++ app/scripts/profile.js
@@ -0,0 +1,20 @@
+// holds the name of our tab button container for selection later in the function
+var tabsContainer = '.user-profile-tabs-container';
+function selectTabHandler(event) {
+   'use strict';
+   var $tab = $(this);
+   $(tabsContainer + ' li').removeClass('active');
+   $tab.parent().addClass('active');
+   var selectedTabName = $tab.attr('href');
+//   alert(selectedTabName);
+   $('.tab-pane').addClass('hidden');
+   $(selectedTabName).removeClass('hidden');
+   event.preventDefault();
+}
+if (document.URL.match(/\/profile.html/)) {
+   $(document).ready(function tabsClick() {
+      var $tabs = $(tabsContainer + ' a');
+      $tabs.click(selectTabHandler);
+      $tabs[0].click();
+   });
+}
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -307,6 +307,7 @@ $(document).ready(function documentReadyR() {
 require('./landing');
 require('./collection');
 require('./album');
+require('./profile');
 
 });
 
@@ -418,6 +419,30 @@ $('.logo ').click(function logoClick() {
   $(this).fadeOut(2000);
 });
 });
+
+});
+
+;require.register("scripts/profile", function(exports, require, module) {
+// holds the name of our tab button container for selection later in the function
+var tabsContainer = '.user-profile-tabs-container';
+function selectTabHandler(event) {
+   'use strict';
+   var $tab = $(this);
+   $(tabsContainer + ' li').removeClass('active');
+   $tab.parent().addClass('active');
+   var selectedTabName = $tab.attr('href');
+//   alert(selectedTabName);
+   $('.tab-pane').addClass('hidden');
+   $(selectedTabName).removeClass('hidden');
+   event.preventDefault();
+}
+if (document.URL.match(/\/profile.html/)) {
+   $(document).ready(function tabsClick() {
+      var $tabs = $(tabsContainer + ' a');
+      $tabs.click(selectTabHandler);
+      $tabs[0].click();
+   });
+}
 
 });
 
