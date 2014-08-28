3d9d9d1bbac24c53203c2f71e106dc2c580ae10c
rsperberg <rsperberg@gmail.com>
2014-08-28 22:00:12 +0000

    added timecode filter to play-bar ch46

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -311,4 +311,30 @@ blocJams.directive('slider', ['$document', function() {
 
         }  // link: function
     };  // return (within slider)
-}]);  //   blocJams.directive('slider'...
+}]);
+//   blocJams.directive('slider'...
+
+//
+blocJams.filter('timecode', function() {
+    return function(seconds) {
+
+        // Returned when no time is provided
+        if (Number.isNaN(seconds)) {
+            return '-:--';
+        }
+
+        // make it a whole number
+        var wholeSeconds = Math.floor(seconds);
+        var minutes = Math.floor(wholeSeconds / 60);
+        var leftoverSeconds = wholeSeconds % 60;
+        var output = minutes + ':';
+
+        // zero pad seconds, so 9 seconds should be :09
+        if (leftoverSeconds < 10) {
+            output += '0';
+        }
+        output += leftoverSeconds;
+        return output;
+    };
+});
+// blocJams.filter('timecode'
