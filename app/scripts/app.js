40e0921addecd8b2f1f8582ee8df06eec3ba96b4
rsperberg <rsperberg@gmail.com>
2014-09-10 21:57:29 -0400

    made changes to fix flipping album covers and appearance

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -61,7 +61,24 @@ ccJukebox.controller('Landing.controller', ['$scope', function($scope) {
         }
     }; //- shuffle()
 
-       var albumsArray = [
+ var albumsArray = [
+[
+'/images/album-placeholders/album-1.jpg',
+'/images/album-placeholders/album-2.jpg',
+'/images/album-placeholders/album-3.jpg'
+],
+[
+'/images/album-placeholders/album-4.jpg',
+'/images/album-placeholders/album-5.jpg',
+'/images/album-placeholders/album-6.jpg'
+],
+[
+'/images/album-placeholders/album-7.jpg',
+'/images/album-placeholders/album-8.jpg',
+'/images/album-placeholders/album-9.jpg'
+]
+];
+/*      var albumsArray = [
         '/images/album-placeholders/album-1.jpg',
         '/images/album-placeholders/album-2.jpg',
         '/images/album-placeholders/album-3.jpg',
@@ -71,7 +88,7 @@ ccJukebox.controller('Landing.controller', ['$scope', function($scope) {
         '/images/album-placeholders/album-7.jpg',
         '/images/album-placeholders/album-8.jpg',
         '/images/album-placeholders/album-9.jpg'
-   ];
+   ];    */
 $scope.albumURLs = albumsArray;
 $scope.titleTextClicked = shuffle(albumsArray);
 
